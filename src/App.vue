5555bb0b8a70f7440d185cc791f8805d55627d04
Naowal Siripatana <way.serongang@gmail.com>
2020-03-14 09:22:53 +0700

    Up index.html , App.vue and nav.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navigation/>
+    <router-view></router-view>
+    <Footer/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navigation from './components/includes/nav.vue'
+import Footer from './components/includes/footer.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navigation,
+    Footer
   }
 }
 </script>
